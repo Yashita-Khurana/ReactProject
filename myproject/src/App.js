99d5aa8f4418fa3f2d80b0483ddfafeb517c2305
Himanshu <75906597+himanshu-1808@users.noreply.github.com>
2021-05-11 20:17:36 +0530

    test

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import BootstrapCarousel from './components/BootstrapCarousel'
 import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <BootstrapCarousel/>
     </div>
   );
 }
