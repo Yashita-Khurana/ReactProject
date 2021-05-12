b51cb3ab8d8357ae0971d6020b874cbfb69b48b7
Himanshu <75906597+himanshu-1808@users.noreply.github.com>
2021-05-12 19:16:51 +0530

    About us page done

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,13 +1,14 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Aboutus from "./components/Aboutus"
 import Test from './components/Test'
 
 function App() {
   return (
     <div className="App">
-  
-    <Test/>
+      <Test/> 
+  <Aboutus/>
+     
     
     </div>
   );
---
+++ myproject/src/components/Aboutus.js
@@ -0,0 +1,183 @@
+import React from 'react'
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import WorkIcon from '@material-ui/icons/Work';
+import EcoIcon from '@material-ui/icons/Eco';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import BuildIcon from '@material-ui/icons/Build';
+function Aboutus() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light py-3">
+                <div className="container"><a href="#" className="navbar-brand d-flex align-items-center"> <i className="fa fa-snowflake-o fa-lg text-primary mr-2"></i><strong>Snowflake</strong></a>
+                    <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" className="navbar-toggler"><span className="navbar-toggler-icon"></span></button>
+                    <div id="navbarSupportedContent" className="collapse navbar-collapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active"><a href="#" className="nav-link font-italic"> Home </a></li>
+                            <li className="nav-item active"><a href="#" className="nav-link font-italic"> About </a></li>
+                            <li className="nav-item active"><a href="#" className="nav-link font-italic"> Services </a></li>
+                            <li className="nav-item active"><a href="#" className="nav-link font-italic"> Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div className="bg-light">
+                <div className="container py-5">
+                    <div className="row h-100 align-items-center py-5">
+                        <div className="col-lg-6">
+                            <h1 className="display-4">About us page</h1>
+                            <p className="lead text-muted mb-0">Create a minimal about us page using Bootstrap 4.</p>
+                            <p className="lead text-muted">Snippet by <a href="https://bootstrapious.com/snippets" className="text-muted">
+                                <u>Bootstrapious</u></a>
+                            </p>
+                        </div>
+                        <div className="col-lg-6 d-none d-lg-block"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834136/illus_kftyh4.png" alt="" className="img-fluid" /></div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bg-white py-5">
+                <div className="container py-5">
+                    <div className="row align-items-center mb-5">
+                        <div className="col-lg-6 order-2 order-lg-1"><i className="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+                            <h2 className="font-weight-light">Lorem ipsum dolor sit amet</h2>
+                            <p className="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><a href="#" className="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+                        </div>
+                        <div className="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834139/img-1_e25nvh.jpg" alt="" className="img-fluid mb-4 mb-lg-0" /></div>
+                    </div>
+                    <div className="row align-items-center">
+                        <div className="col-lg-5 px-5 mx-auto"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834136/img-2_vdgqgn.jpg" alt="" className="img-fluid mb-4 mb-lg-0" /></div>
+                        <div className="col-lg-6"><i className="fa fa-leaf fa-2x mb-3 text-primary"></i>
+                            <h2 className="font-weight-light">Lorem ipsum dolor sit amet</h2>
+                            <p className="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><a href="#" className="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bg-light py-5">
+                <div className="container py-5">
+                    <div className="row mb-4">
+                        <div className="col-lg-5">
+                            <h2 className="display-4 font-weight-light">Our team</h2>
+                            <p className="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+
+                    <div className="row text-center">
+
+                        <div className="col-xl-3 col-sm-6 mb-5">
+                            <div className="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834132/avatar-4_ozhrib.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                                <h5 className="mb-0">Manuella Nevoresky</h5><span className="small text-uppercase text-muted">CEO - Founder</span>
+                                <ul className="social mb-0 list-inline mt-3">
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-facebook-f"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-twitter"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-instagram"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="col-xl-3 col-sm-6 mb-5">
+                            <div className="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834130/avatar-3_hzlize.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                                <h5 className="mb-0">Samuel Hardy</h5><span className="small text-uppercase text-muted">CEO - Founder</span>
+                                <ul className="social mb-0 list-inline mt-3">
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-facebook-f"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-twitter"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-instagram"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="col-xl-3 col-sm-6 mb-5">
+                            <div className="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834133/avatar-2_f8dowd.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                                <h5 className="mb-0">Tom Sunderland</h5><span className="small text-uppercase text-muted">CEO - Founder</span>
+                                <ul className="social mb-0 list-inline mt-3">
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-facebook-f"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-twitter"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-instagram"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+
+                        <div className="col-xl-3 col-sm-6 mb-5">
+                            <div className="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834133/avatar-1_s02nlg.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                                <h5 className="mb-0">John Tarly</h5><span className="small text-uppercase text-muted">CEO - Founder</span>
+                                <ul className="social mb-0 list-inline mt-3">
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-facebook-f"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-twitter"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-instagram"></i></a></li>
+                                    <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="container-fluid text-center">
+                <h2>SERVICES</h2>
+                <h4>What we offer</h4>
+                <br></br>
+                <div className="row slideanim">
+                    <div className="col-sm-4">
+                        <span className="glyphicon glyphicon-off logo-small"></span>
+                        <PowerSettingsNewIcon/>
+                        <h4>POWER</h4>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                    </div>
+                    <div className="col-sm-4">
+                        <span className="glyphicon glyphicon-heart logo-small"></span>
+                        <FavoriteIcon/>
+                        <h4>LOVE</h4>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                    </div>
+                    <div className="col-sm-4">
+                        <span className="glyphicon glyphicon-lock logo-small"></span>
+                        <WorkIcon/>
+                        <h4>JOB DONE</h4>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                    </div>
+                </div>
+                <br></br>
+                    <div className="row slideanim">
+                        <div className="col-sm-4">
+                            <span className="glyphicon glyphicon-leaf logo-small"></span>
+                            <EcoIcon/>
+                            <h4>GREEN</h4>
+                            <p>Lorem ipsum dolor sit amet..</p>
+                        </div>
+                        <div className="col-sm-4">
+                            <span className="glyphicon glyphicon-certificate logo-small"></span>
+                            <VerifiedUserIcon/>
+                            <h4>CERTIFIED</h4>
+                            <p>Lorem ipsum dolor sit amet..</p>
+                        </div>
+                        <div className="col-sm-4">
+                            <span className="glyphicon glyphicon-wrench logo-small"></span>
+                            <BuildIcon/>
+                            <h4>HARD WORK</h4>
+                            <p>Lorem ipsum dolor sit amet..</p>
+                        </div>
+                    </div>
+                 
+            </div>
+
+                    <footer className="bg-light pb-5">
+                        <div className="container text-center">
+                            <p className="font-italic text-muted mb-0">&copy; Copyrights Company.com All rights reserved.</p>
+                        </div>
+                    </footer>
+        </div>
+    )
+}
+
+export default Aboutus
